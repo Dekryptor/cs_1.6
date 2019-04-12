52bf6dfb2fd61e40480d89f9af2668d04b77c3d8
Kraen byskov <kraenbyskov@gmail.com>
2019-04-12 09:18:59 +0200

    It works but not really

--- client/state/player/createPlayer.js
+++ client/state/player/createPlayer.js
@@ -1,7 +1,7 @@
 const createPlayer = (x, y, game) => {
-  const sprite = game.add.sprite(x, y, "car");
-  game.physics.p2.enable(sprite, false);
-  return sprite;
-};
+  const sprite = game.add.sprite(x, y, 'car')
+  game.physics.p2.enable(sprite, false)
+  return sprite
+}
 
-export default createPlayer;
+export default createPlayer
